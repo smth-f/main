a510b7fe05a834a8606bfba85dc198f72edce25d
smth <smth.github@gmail.com>
2023-11-18 21:59:43 +0200

    1-12 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,32 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task #1");
-        System.out.println(task1(new int[] {3,5,-1,99,6})); //true - -1
+        System.out.println(task1(new int[] {3,5,-1,99,6})); //true res - -1
         System.out.println("Task #2");
-        System.out.println(Arrays.toString(task2(new int[]{3, 5, -1, 99, 6}))); //true - [-1, 3, 5, 6, 99]
+        System.out.println(Arrays.toString(task2(new int[]{3, 5, -1, 99, 6}))); //true res - [-1, 3, 5, 6, 99]
         System.out.println("Task #3");
-        System.out.println(task3(5)); //true - 120
+        System.out.println(task3(5)); //true res - 120
         System.out.println("Task #4");
-        System.out.println(task4(new int[] {4,1,65,2,9,13,32})); //true - 3
+        System.out.println(task4(new int[] {4,1,65,2,9,13,32})); //true res - 3
         System.out.println("Task #5");
-        System.out.println(task5(5)); //true - true
+        System.out.println(task5(5)); //true res - true
         System.out.println("Task #6");
-        System.out.println(task6("Hello")); //true - 2
+        System.out.println(task6("Hello")); //true res - 2
         System.out.println("Task #7");
-        System.out.println(task7(new int[] {4,3,5})); //true - 4
+        System.out.println(task7(new int[] {4,3,5})); //true res - 4
         System.out.println("Task #8");
-        System.out.println(task8(12,16)); //true - 4
+        System.out.println(task8(12,16)); //true res - 4
+        System.out.println("Task #9");
+        System.out.println(task9(6)); //true res - 8
+        System.out.println("Task #10");
+        System.out.println(task10(new int[] {1,2,5,7,10})); //true res - [3, 4, 6, 8, 9]
+        System.out.println("Task #11");
+        System.out.println(task11(14541)); //true res - true
+        System.out.println("Task #12");
+        System.out.println(task12("Hello","loHel")); //true res - true
     }
     public static int task1(int[] ar){
         Arrays.sort(ar);
@@ -91,5 +100,54 @@ public class Main {
             }
         }
         return 1;
+    }
+    public static int task9(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return task9(n-1) + task9(n-2);
+        }
+    }
+    public static ArrayList<Integer> task10(int[] ar){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Arrays.sort(ar);
+        for (int i = 1; i < ar.length; i++) {
+            int diff = ar[i] - ar[i - 1];
+            if (diff > 1) {
+                for (int j = 1; j < diff; j++) {
+                    arrayList.add(ar[i - 1] + j);
+                }
+            }
+        }
+        return arrayList;
+    }
+    public static boolean task11(int n){
+        String s = String.valueOf(n);
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static boolean task12(String s1, String s2){
+        char[] ar1 = s1.toCharArray();
+        char[] ar2 = s2.toCharArray();
+        if (ar1.length != ar2.length){
+            return false;
+        }
+        for (char cr : ar1) {
+            for (int j = 0; j < ar2.length; j++) {
+                if (cr == ar2[j]) break;
+                else if (j == ar2.length - 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
